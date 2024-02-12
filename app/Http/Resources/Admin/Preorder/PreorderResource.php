2c38c13fce66a08121eab792395a972e00a2decc
Mochamad Karsa Syaifudin Jupri <karsaaif87@gmail.com>
2024-02-12 16:39:56 +0700

    fix preorder

--- app/Http/Resources/Admin/Preorder/PreorderResource.php
+++ app/Http/Resources/Admin/Preorder/PreorderResource.php
@@ -56,7 +56,7 @@ class PreorderResource extends JsonResource
         }
 
         $customerAddress = null;
-        if ($this->customer) {
+        if ($this->customer_address) {
             $customerAddress = [
                 'id' => $this->customer_address->id,
                 'name' => $this->customer_address->name,
